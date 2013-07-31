041f6f4457175cc1bdc7e1a4422bd4a7587f7180
samzhang111 <szhang1@swarthmore.edu>
2013-07-31 15:49:54 -0400

    tester.py

---
+++ tester.py
@@ -0,0 +1,289 @@
+import BeautifulSoup
+from BeautifulSoup import BeautifulSoup as bs
+from math import sqrt
+import sys
+import re
+import argparse
+
+def mean(li):
+    """
+    mean(list)
+    returns mean of a list of integers (float)
+
+    """
+
+    return float(sum(li))/len(li)
+
+def std(li):
+    """
+    std(list)
+
+    returns standard deviation of list of integers (float)
+
+    """
+    m = mean(li)
+    return sqrt(mean([abs(x-m) for x in li]))
+
+def strip_links(L):
+    """strip_links(L)
+    Takes all BeautifulSoup tags at a certain depth level.
+    Returns the list, without the links.
+
+    """
+    return filter(lambda x: x.name!='a', L)
+
+def get_tags_by_depth(soup):
+    """get_tags_by_depth(soup)
+    Takes a BeautifulSoup object. Returns a list [d0, d1, ..., dn],
+    where di is a list of all tags at that given depth level.
+    Eg d0=[head, body]
+    d1=[meta, table, script]
+    Note: the lists will not print out the names, because they contain all the children below them as well.
+
+    """
+    rawtags = soup.findAll()
+    if not rawtags:
+        return []
+    daddio = rawtags[0]
+    siblings = daddio.fetchNextSiblings()
+    parents = [daddio] + [s for s in siblings]
+    
+    all_tags= []
+    for i in range(len(parents)):
+        tags_by_depth = [parents[i].findChildren(recursive=False)]
+        d = 1
+        while True:
+            tags = []
+            for t in tags_by_depth[d-1]:
+                tags.extend(t.findChildren(recursive=False))
+        
+            tags = filter(None, tags)
+            if tags:
+                tags_by_depth.append(tags)
+            else:
+                all_tags.append(tags_by_depth)  
+                break
+    
+            d+=1
+    return map(None, *all_tags)
+
+def tag_distribution(L):
+    """tag_distribution(L):
+    Takes all BeautifulSoup tags at a certain depth level.
+    Returns a dictionary of counts for each tag-attr.
+    Strips all links.
+    Replaces all series of numbers with a single x.
+
+    E.g. [BeautifulSoup("<div class=post32124></div>")] returns {'div-postx': 1}
+
+    """
+    ud = {}
+    for x in L:
+        n = name_attr(x)
+        if n:
+            if n in ud.keys():
+                ud[n] += 1
+            else:
+                ud[n] = 1
+    return ud
+
+def same_tags(L1, L2):
+    """same_tags(L1, L2)
+    Takes two lists of BeautifulSoup tags
+    Returns True if all tags in one list are in the other.
+    
+    """ 
+    
+    s1 = set(tag_distribution(L1).keys())
+    s2 = set(tag_distribution(L2).keys())
+
+
+    return all(k1 in s2 for k1 in s1) and
+            all(k2 in s1 for k2 in s2)
+
+
+def similarity(L1, L2):
+    """ similarity(L1, L2)
+    Takes two lists of BeautifulSoup tags
+    Returns the number of UNIQUE name-attr pairs that are not shared by both, normalized by total number of unique pairs
+
+    """
+    d1 = tag_distribution(L1)
+    d2 = tag_distribution(L2)
+    mismatches = 0
+    for k1 in d1.keys():
+        if k1 not in d2.keys():
+            mismatches += 1 
+
+    for k2 in d2.keys():
+        if k2 not in d1.keys():
+            mismatches += 1
+
+    return float(mismatches)/(len(d1.keys())+len(d2.keys()))
+
+
+def name_attr(tag, strip_nums=1, strip_orphans=1, strip_links=1):
+    """name_attr(tag, strip_nums=1, strip_orphans=1, strip_links=1)
+    Takes a BeautifulSoup tag.
+    Returns a string formatted "name-first_attribute-second_attribute".
+    E.g. [<div class="post" style="font-weight:normal"></div>] returns "div-post-font-weight:normal"
+    
+    strip_nums: strips the numbers from the attribute, replacing any series of them with a single "x"
+    E.g. [<div class=post232></div>] returns "div-postx"
+
+    strip_orphans: returns an empty string for any tag that doesn't have attributes.
+    
+    strip_links: returns an empty string for any link tags.
+    
+    """
+    if not tag:
+        return ""
+    if not strip_links or tag.name!= 'a':
+        if tag.attrs:
+            at = [tag.attrs[i][1] for i in xrange(len(tag.attrs))]
+            at = "-".join(at)
+            if strip_nums:
+                at = re.sub(r"\d+","x", at)
+            return "%s-%s"%(tag.name, at)
+        elif not strip_orphans:
+            return tag.name
+    return ""
+
+def pn(L):
+    """pn(L):
+    (print name)
+    Takes all BeautifulSoup tags at a certain depth level.
+    Calls name_attr() on all of them.
+
+    """
+    return filter(None, map(name_attr, L))
+
+def of(name, num, suffix=""):
+    soups = []
+    for i in xrange(1, int(num)+1):
+        soups.append(bs(open("%s%s%s"%(name,str(i),suffix)).read()))
+    return soups
+
+def get_outliars(dic):
+    """get_outliars(dictionary)
+    Takes a dictionary where the values are integer counts.
+    Returns the keys where the values are two standard deviations above the mean.
+
+    """
+    m = mean(dic.values())
+    s = std(dic.values())
+    k = filter(lambda x: x[1] > m + 2*s, dic.items())
+    if k:
+        return zip(*k)[0]
+
+def get_tags_by_string(na, L):
+    """get_tags_by_string(string, list of BS tags)
+    Takes a name-attr string (as specified in name_attr()) and returns tags that match
+
+    """
+    
+    return filter(lambda x: name_attr(x)==na, L)
+
+def get_outliars_by_page(page_tbd, depth):
+    """get_outliars_by_page(list of tags_by_depth, depth, index)
+    Returns all tags that appear disproportionately often.
+    """
+    if not outliars:
+        return []
+    out_tags = (get_tags_by_string(x, tbds[d][i]) for x in outliars)
+    
+    out_tags_no_boring = []
+    for i, x in enumerate(out_tags):
+        if any(not e.text for e in x):
+            continue
+        if all(e.text==x[0].text for e in x):
+            continue
+        out_tags_no_boring.append(x)
+
+    return out_tags_no_boring
+
+def strip_useless_tags(tbd):
+
+     
+    children = tag.findChildren()
+    for c in children:
+        if True:
+            pass
+        
+
+def pprint_file(obd):
+    """pprint_file(outliars_by_depth)
+    pretty prints element of list returned by call to get_outliars_by_depth().
+
+    """
+    matches = map(None, *obd)
+    for i,m in enumerate(matches):
+        print "------%d------" % i
+        for x in m:
+            print x
+            print "..."
+
+
+#### Todo:
+#### Finish structure_stats, use it to compute simlarity metrics,
+#### refer to data structure drawing if you get confused
+
+def structure_stats(T):
+    """Takes BeautifulSoup tag structure
+    Returns
+        1) their maximum depth
+        2) the total number of nested tags
+    
+    """
+    similarity = 0
+
+    # Both are not the same type, eg NavigableString and Tag, so return 0
+    if type(T1) != type(T2):
+        return 0
+
+    # Neither are tags, but are same type, so return 1 for max similarity
+    if type(T1) != BeautifulSoup.Tag:
+        return 1
+    
+    remaining_tags = filter(lambda x: type(x) == BeautifulSoup.Tag, T1)
+    depth+=1
+    
+
+if len(sys.argv) < 4:
+    print "usage: python tester.py dir # suffix"
+
+soups = of(sys.argv[1], sys.argv[2], sys.argv[3])
+
+dbfs = map(get_tags_by_depth, soups)
+# dbfs = Depth by files [f1:[d1, d2, ...], f2:[d1, d2, ...],...]
+# f_i and f_j can be different lengths, depending on their max depth
+# len(dbfs) is always the number of pages
+
+fbds = zip(*dbfs)
+# fbds = Files by depth [d1:[f1, f2, ...], d2[f1, f2, ...], ...]
+# all d_i are the same length, because they all contain equal # files
+# each f_i entry is actually a BeautifulSoup tag, so fbds[0][0] might contain
+# "[<HTML>...</HTML>]", for example.
+# len(dbfs) is the maximum tag depth of all the pages.
+
+if len(fbds) < 3:
+    print "Min depth not achieved."
+    sys.exit()
+print "Beginning comparison at depth level", fbds/3
+
+for i in range(fbds/3, len(fbds)-1):
+    if not all(same_tags(x1, x2) for x1 in fbds[i] for x2 in fbds[i]):
+        print "Not all tags at depth level %d are the same"%i
+        break
+    """
+    sim_matrix = [similarity(x1, x2) for x1 in fbds[i] for x2 in fbds[i]]
+    print i, std(map(len, fbds[i]))
+    if sum(sim_matrix) != 0:
+        print "No longer closely related."
+        break
+    """
+t1 = [get_outliars_by_depth(fbds, i-1, x) for x in xrange(len(fbds[i-1]))]
+t2 = [get_outliars_by_depth(fbds, i  , x) for x in xrange(len(fbds[i]))]
+t3 = [get_outliars_by_depth(fbds, i+1, x) for x in xrange(len(fbds[i+1]))]
+
+strip_useless_tags_from_the_heart_of_it(t2)
